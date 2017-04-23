3684ab422a6af2951786231c5c103b7837df1c23
Nao Ohira <nao.ohira@aareon.fr>
2017-04-23 21:40:07 +0200

    Fix visibility

--- src/message/Message.ts
+++ src/message/Message.ts
@@ -4,14 +4,13 @@ import { IPublisher } from '../interface/IPublisher';
 import { Topic } from '../type/Topic';
 
 export class Message<T extends IContent> implements IMessage<T> {
-
     public constructor(publisher : IPublisher<T>, topic : Topic, content? : T) {
         this.publisher = publisher;
         this.topic     = topic;
         this.content   = content;
     }
 
-    readonly publisher : IPublisher<T>;
-    readonly topic : Topic;
-    readonly content : T;
+    public readonly publisher : IPublisher<T>;
+    public readonly topic : Topic;
+    public readonly content : T;
 }
